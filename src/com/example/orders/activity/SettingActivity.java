d7e347bca2f36fb2c67e3da9c82fee3569024de5
Hy490950439 <490950439@qq.com>
2015-05-14 12:27:08 +0800

    HY

--- src/com/example/orders/activity/SettingActivity.java
+++ src/com/example/orders/activity/SettingActivity.java
@@ -1,24 +1,33 @@
 package com.example.orders.activity;
 
 import com.example.orders.R;
+import com.example.orders.setting.InformationActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class SettingActivity extends Activity {
-
+	public static SettingActivity instance = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		instance = this;
 		setContentView(R.layout.activity_setting);
+		TextView txtViewRegister = (TextView)findViewById(R.id.gerenxinxi);
+		txtViewRegister.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			// TODO Auto-generated method stub
+				Intent intent = new Intent(SettingActivity.this,InformationActivity.class);
+				startActivity(intent);
+			}
+			});
+		}
 	}
+	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.setting, menu);
-		return true;
-	}
+	
 
-}
