e92779b61aa64fadda0d739360cc2cd089fc624c
Hy490950439 <490950439@qq.com>
2015-05-18 17:08:09 +0800

    HY

--- src/com/example/orders/setting/InformationActivity.java
+++ src/com/example/orders/setting/InformationActivity.java
@@ -1,175 +1,172 @@
 package com.example.orders.setting;
 
 import java.io.File;
-import java.util.Date;
 
 import com.example.orders.R;
-
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.Layout;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.TextUtils;
-import android.text.style.AbsoluteSizeSpan;
-import android.text.style.AlignmentSpan;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class InformationActivity extends Activity {
-	private Context mCon;
-	private String TAG = "InformationActivity";
-	private RelativeLayout layout;
-	private ImageView head;
-	public static final String IMAGE_PATH = "";
-	private static String localTempImageFileName = "";
-	private static final int FLAG_CHOOSE_IMG = 5;
-	private static final int FLAG_CHOOSE_PHONE = 6;
-	private static final int FLAG_MODIFY_FINISH = 7;
-	public static final File FILE_SDCARD = Environment
-			.getExternalStorageDirectory();
-	public static final File FILE_LOCAL = new File(FILE_SDCARD, IMAGE_PATH);
-	public static final File FILE_PIC_SCREENSHOT = new File(FILE_LOCAL,
-			"images/screenshots");
+	/* 组件 */
+	private RelativeLayout switchAvatar;
+	private ImageView faceImage;
+
+	private String[] items = new String[] { "选择本地图片", "拍照" };
+	/* 头像名称 */
+	private static final String IMAGE_FILE_NAME = "faceImage.jpg";
+
+	/* 请求码 */
+	private static final int IMAGE_REQUEST_CODE = 0;
+	private static final int CAMERA_REQUEST_CODE = 1;
+	private static final int RESULT_REQUEST_CODE = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.setting_information);
-		mCon = InformationActivity.this;
-		head = (ImageView) findViewById(R.id.main_tab_setting_information_head);
-		// 跳出选择界面
-		layout = (RelativeLayout) findViewById(R.id.main_tab_setting_information_rl);
-		layout.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// 调用选择那种方式的dialog
-				ModifyAvatarDialog modifyAvatarDialog = new ModifyAvatarDialog(
-						mCon) {
-					// 选择本地相册
-					@Override
-					public void doGoToImg() {
-						this.dismiss();
-						Intent intent = new Intent();
-						intent.setAction(Intent.ACTION_PICK);
-						intent.setType("image/*");
-						startActivityForResult(intent, FLAG_CHOOSE_IMG);
-					}
+		switchAvatar = (RelativeLayout) findViewById(R.id.main_tab_setting_information_rl);
+		faceImage = (ImageView) findViewById(R.id.main_tab_setting_information_head);
+		// 设置事件监听
+		switchAvatar.setOnClickListener(listener);
+	}
 
-					// 选择相机拍照
+	private View.OnClickListener listener = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			showDialog();
+		}
+	};
+
+	/**
+	 * 显示选择对话框
+	 */
+	private void showDialog() {
+
+		new AlertDialog.Builder(this)
+				.setTitle("设置头像")
+				.setItems(items, new DialogInterface.OnClickListener() {
+
 					@Override
-					public void doGoToPhone() {
-						this.dismiss();
-						String status = Environment.getExternalStorageState();
-						if (status.equals(Environment.MEDIA_MOUNTED)) {
-							try {
-								localTempImageFileName = "";
-								localTempImageFileName = String
-										.valueOf((new Date()).getTime())
-										+ ".png";
-								File filePath = FILE_PIC_SCREENSHOT;
-								if (!filePath.exists()) {
-									filePath.mkdirs();
-								}
-								Intent intent = new Intent(
-										android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-								File f = new File(filePath,
-										localTempImageFileName);
-								// localTempImgDir和localTempImageFileName是自己定义的名字
-								Uri u = Uri.fromFile(f);
-								intent.putExtra(
-										MediaStore.Images.Media.ORIENTATION, 0);
-								intent.putExtra(MediaStore.EXTRA_OUTPUT, u);
-								startActivityForResult(intent,
-										FLAG_CHOOSE_PHONE);
-							} catch (ActivityNotFoundException e) {
-								//
+					public void onClick(DialogInterface dialog, int which) {
+						switch (which) {
+						case 0:
+							Intent intentFromGallery = new Intent();
+							intentFromGallery.setType("image/*"); // 设置文件类型
+							intentFromGallery
+									.setAction(Intent.ACTION_GET_CONTENT);
+							startActivityForResult(intentFromGallery,
+									IMAGE_REQUEST_CODE);
+							break;
+						case 1:
+
+							Intent intentFromCapture = new Intent(
+									MediaStore.ACTION_IMAGE_CAPTURE);
+							// 判断存储卡是否可以用，可用进行存储
+							if (Tools.hasSdcard()) {
+
+								intentFromCapture.putExtra(
+										MediaStore.EXTRA_OUTPUT,
+										Uri.fromFile(new File(Environment
+												.getExternalStorageDirectory(),
+												IMAGE_FILE_NAME)));
 							}
+
+							startActivityForResult(intentFromCapture,
+									CAMERA_REQUEST_CODE);
+							break;
 						}
 					}
-				};
-				AlignmentSpan span = new AlignmentSpan.Standard(
-						Layout.Alignment.ALIGN_CENTER);
-				AbsoluteSizeSpan span_size = new AbsoluteSizeSpan(25, true);
-				SpannableStringBuilder spannable = new SpannableStringBuilder();
-				String dTitle = getString(R.string.gl_choose_title);
-				spannable.append(dTitle);
-				spannable.setSpan(span, 0, dTitle.length(),
-						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-				spannable.setSpan(span_size, 0, dTitle.length(),
-						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-				modifyAvatarDialog.setTitle(spannable);
-				modifyAvatarDialog.show();
-			}
-		});
+				})
+				.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				}).show();
+
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == FLAG_CHOOSE_IMG && resultCode == RESULT_OK) {
-			if (data != null) {
-				Uri uri = data.getData();
-				if (!TextUtils.isEmpty(uri.getAuthority())) {
-					Cursor cursor = getContentResolver().query(uri,
-							new String[] { MediaStore.Images.Media.DATA },
-							null, null, null);
-					if (null == cursor) {
-						Toast.makeText(mCon, "图片没找到", 0).show();
-						return;
-					}
-					cursor.moveToFirst();
-					String path = cursor.getString(cursor
-							.getColumnIndex(MediaStore.Images.Media.DATA));
-					cursor.close();
-					Log.i(TAG, "path=" + path);
-					Intent intent = new Intent();
-					intent.setClass(InformationActivity.this,
-							CropImageActivity.class);
-					InformationActivity.this.startActivity(intent);
-					intent.putExtra("path", path);
-					startActivityForResult(intent, FLAG_MODIFY_FINISH);
-				} else {
-					Log.i(TAG, "path=" + uri.getPath());
-					Intent intent = new Intent();
-					intent.setClass(InformationActivity.this,
-							CropImageActivity.class);
-					InformationActivity.this.startActivity(intent);
-					intent.putExtra("path", uri.getPath());
-					startActivityForResult(intent, FLAG_MODIFY_FINISH);
-				}
-			}
-		} else if (requestCode == FLAG_CHOOSE_PHONE && resultCode == RESULT_OK) {
-			File f = new File(FILE_PIC_SCREENSHOT, localTempImageFileName);
-			Intent intent = new Intent();
-			intent.setClass(InformationActivity.this, CropImageActivity.class);
-			InformationActivity.this.startActivity(intent);
-			intent.putExtra("path", f.getAbsolutePath());
-			startActivityForResult(intent, FLAG_MODIFY_FINISH);
-		} else if (requestCode == FLAG_MODIFY_FINISH && resultCode == RESULT_OK) {
-			if (data != null) {
-				final String path = data.getStringExtra("path");
-				Log.i(TAG, "截取到的图片路径是 = " + path);
-				Bitmap b = BitmapFactory.decodeFile(path);
-				head.setImageBitmap(b);
+		// 结果码不等于取消时候
+		if (resultCode != RESULT_CANCELED) {
 
+			switch (requestCode) {
+			case IMAGE_REQUEST_CODE:
+				startPhotoZoom(data.getData());
+				break;
+			case CAMERA_REQUEST_CODE:
+				if (Tools.hasSdcard()) {
+					File tempFile = new File(
+							Environment.getExternalStorageDirectory()
+									+ IMAGE_FILE_NAME);
+					startPhotoZoom(Uri.fromFile(tempFile));
+				} else {
+					Toast.makeText(InformationActivity.this, "未找到存储卡，无法存储照片！",
+							Toast.LENGTH_LONG).show();
+				}
+
+				break;
+			case RESULT_REQUEST_CODE:
+				if (data != null) {
+					getImageToView(data);
+				}
+				break;
 			}
 		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
+	/**
+	 * 裁剪图片方法实现
+	 * 
+	 * @param uri
+	 */
+	public void startPhotoZoom(Uri uri) {
+
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		// 设置裁剪
+		intent.putExtra("crop", "true");
+		// aspectX aspectY 是宽高的比例
+		intent.putExtra("aspectX", 0.5);
+		intent.putExtra("aspectY", 0.5);
+		// outputX outputY 是裁剪图片宽高
+		intent.putExtra("outputX", 320);
+		intent.putExtra("outputY", 320);
+		intent.putExtra("return-data", true);
+		startActivityForResult(intent, 2);
+	}
+
+	/**
+	 * 保存裁剪之后的图片数据
+	 * 
+	 * @param picdata
+	 */
+	private void getImageToView(Intent data) {
+		Bundle extras = data.getExtras();
+		if (extras != null) {
+			Bitmap photo = extras.getParcelable("data");
+			@SuppressWarnings("deprecation")
+			Drawable drawable = new BitmapDrawable(photo);
+			faceImage.setImageDrawable(drawable);
+		}
+	}
 }
