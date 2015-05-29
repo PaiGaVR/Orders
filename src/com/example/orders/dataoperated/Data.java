28ccf15b2e29235721a91236e48ec43a7dd159b3
GeXiaoDong <421536265@qq.com>
2015-05-29 11:31:40 +0800

    GXD

---
+++ src/com/example/orders/dataoperated/Data.java
@@ -0,0 +1,55 @@
+package com.example.orders.dataoperated;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.orders.entity.Orders;
+
+public class Data {
+	private List<Orders> pushOrdersList = null;
+	private List<Orders> getOrdersList = null;
+	private List<Orders> pushingOrdersList = null;
+	private List<Orders> overOrdersList = null;
+	
+	public Data() {
+		initPushOrdersList();
+		initGetOrdersList();
+		initPushingOrdersList();
+		initOverOrdersList();
+	}
+	
+	/**
+	 * 下单界面数据初始化
+	 */
+	private void initPushOrdersList() {
+		pushOrdersList = new ArrayList<Orders>();
+	}
+	
+	private void initGetOrdersList() {
+		getOrdersList = new ArrayList<Orders>();
+	}
+	
+	private void initPushingOrdersList() {
+		pushingOrdersList = new ArrayList<Orders>();
+	}
+	
+	private void initOverOrdersList() {
+		overOrdersList = new ArrayList<Orders>();
+	}
+
+	public List<Orders> getPushOrdersList() {
+		return pushOrdersList;
+	}
+
+	public List<Orders> getGetOrdersList() {
+		return getOrdersList;
+	}
+
+	public List<Orders> getPushingOrdersList() {
+		return pushingOrdersList;
+	}
+
+	public List<Orders> getOverOrdersList() {
+		return overOrdersList;
+	}
+}
---
+++ src/com/example/orders/dataoperated/OrdersOperated.java
@@ -0,0 +1,84 @@
+package com.example.orders.dataoperated;
+
+import java.util.List;
+
+import com.example.orders.entity.Orders;
+
+public class OrdersOperated {
+	public static OrdersOperated ordersOperated = null;
+	
+	private List<Orders> pushOrdersList = null;
+	private List<Orders> getOrdersList = null;
+	private List<Orders> pushingOrdersList = null;
+	private List<Orders> overOrdersList = null;
+	
+	private Data data;
+	
+	private OrdersOperated(){
+		data = new Data();
+	}
+	
+	public static OrdersOperated getInstance(){
+		if (ordersOperated == null)
+			ordersOperated = new OrdersOperated();
+		
+		return ordersOperated;
+	}
+	
+	/**
+	 * 写order
+	 * @param order
+	 * @return PushOrders列表
+	 */
+	public List<Orders> writeOrder(Orders order){
+		if (pushOrdersList == null)
+			pushOrdersList = data.getPushOrdersList();
+		
+		pushOrdersList.add(0, order);
+		return pushOrdersList;
+	}
+	
+	/**
+	 * 读PushOrders列表
+	 * @return PushOrders列表
+	 */
+	public List<Orders> getPushOrdersList(){
+		if (pushOrdersList == null)
+			pushOrdersList = data.getPushOrdersList();
+		
+		return pushOrdersList;
+	}
+	
+	/**
+	 * 读GetOrders列表
+	 * @return GetOrders列表
+	 */
+	public List<Orders> getGetOrdersList(){
+		if (getOrdersList == null)
+			getOrdersList = data.getGetOrdersList();
+		
+		return getOrdersList;
+	}
+	
+	/**
+	 * 读PushingOrders列表
+	 * @return PushingOrders列表
+	 */
+	public List<Orders> getPushingOrders(){
+		if (pushingOrdersList == null)
+			pushingOrdersList = data.getPushingOrdersList();
+		
+		return pushingOrdersList;
+	}
+	
+	/**
+	 * 读OverOrders列表
+	 * @return OverOrders列表
+	 */
+	public List<Orders> getOverOrders(){
+		if (overOrdersList == null)
+			overOrdersList = data.getOverOrdersList();
+		
+		return overOrdersList;
+	}
+}
